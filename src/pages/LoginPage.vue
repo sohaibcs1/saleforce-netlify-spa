d9bc61ba9b1b7da642d1cd79fd2c1ac22d2b97ec
Muhammad Sohaib <43402659+sohaibcs1@users.noreply.github.com>
2023-02-12 20:43:54 +0500

    jksd

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -6,10 +6,7 @@
     </ul>
   </div>
 </template>
-
 <script>
-import axios from "axios";
-
 export default {
   data() {
     return {
@@ -17,11 +14,6 @@ export default {
     };
   },
   mounted() {
-    // REAL_TIME('update_frame', (res) => {
-    //   this.priority1 = res;
-    // });
-    // API('home.getNumbers', {});
-    // API("auth.createCam", { last_checked: "12:30 PM", full_name: "Michael Oddoye",email_address:"Michael.Oddoye@gmail.com",job_title:"Team Lead",company:"Salesforce"});
     this.getData();
   },
   methods: {
@@ -30,29 +22,24 @@ export default {
     },
     async getData() {
       try {
-        // Redirect the user to the authorization endpoint
-
-        // https://login.salesforce.com/services/oauth2/authorize?client_id=client_id&redirect_uri=redirect_uri&response_type=code
-        // https://login.salesforce.com/services/oauth2/authorize?client_id=3MVG9DREgiBqN9Wmlb9Hnby7AUNpQD9QxvcveUs7jKTZ8cuRwCe.ZRPqPhSWbUXkVXsASwuxH2kgsD7dGsxMz&redirect_uri=https://13ef-176-32-21-209.eu.ngrok.io/callback&response_type=code
-        // Get the authorization code from the query parameters
         const urlParams = new URLSearchParams(window.location.search);
         const code = urlParams.get("code");
 
-        // Use the authorization code to request an access token
-        const accessTokenResponse = await axios.post(
+        const accessTokenResponse = await fetch(
           "https://login.salesforce.com/services/oauth2/token",
           {
-            grant_type: "authorization_code",
-            client_id:
-              "3MVG9DREgiBqN9Wmlb9Hnby7AUEJb.iiEalt328_cNrVl6IHo8mzw1RpoyJWeypdCAZnm9zrb_7..azvf73l3",
-            client_secret:
-              "62F4A81CC57C95EA82B3B824035D2ABCB732B53154E2F96A62B685FE66A985D6",
-            redirect_uri: "https://zingy-gingersnap-8c57f7.netlify.app/#/login",
-            code: code,
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: `grant_type=authorization_code&client_id=3MVG9DREgiBqN9Wmlb9Hnby7AUEJb.iiEalt328_cNrVl6IHo8mzw1RpoyJWeypdCAZnm9zrb_7..azvf73l3&client_secret=62F4A81CC57C95EA82B3B824035D2ABCB732B53154E2F96A62B685FE66A985D6&redirect_uri=https://zingy-gingersnap-8c57f7.netlify.app/#/login&code=${code}`,
           }
         );
-        const accessToken = accessTokenResponse.data.access_token;
-        const apiResponse = await axios.get(
+
+        const accessTokenData = await accessTokenResponse.json();
+        const accessToken = accessTokenData.access_token;
+
+        const apiResponse = await fetch(
           "https://UM8.salesforce.com/services/data/v57.0/query",
           {
             headers: {
@@ -64,7 +51,8 @@ export default {
           }
         );
 
-        this.data = apiResponse.data.records;
+        const apiData = await apiResponse.json();
+        this.data = apiData.records;
       } catch (error) {
         console.error(error);
       }
