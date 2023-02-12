130390d9a0cfca54187fa97a27902448183f7ea8
Muhammad Sohaib <43402659+sohaibcs1@users.noreply.github.com>
2023-02-12 21:11:50 +0500

    fd

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <q-btn @click="getav">Get Data</q-btn>
-    {{ data }}
+    <ul v-if="data">
+      <li v-for="item in data" :key="item">{{ item.Name }}</li>
+    </ul>
   </div>
 </template>
 <script>
@@ -28,7 +30,7 @@ export default {
           {
             method: "POST",
             headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
+              "Content-Type": "application/json",
             },
             body: `grant_type=authorization_code&client_id=3MVG9DREgiBqN9Wmlb9Hnby7AUEJb.iiEalt328_cNrVl6IHo8mzw1RpoyJWeypdCAZnm9zrb_7..azvf73l3&client_secret=62F4A81CC57C95EA82B3B824035D2ABCB732B53154E2F96A62B685FE66A985D6&redirect_uri=https://zingy-gingersnap-8c57f7.netlify.app/#/login&code=${code}`,
           }
